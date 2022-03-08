5480f323a97a0b0030055dcf17fa66bf234db488
Sakib Ahmed <sakib.ahmed@onepeloton.com>
2022-03-08 16:11:57 -0500

    fix

--- src/Components/AddNewGame.js
+++ src/Components/AddNewGame.js
@@ -1,8 +1,21 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
 
 const AddNewGame = () => {
-  return <Container>hello</Container>;
+  return (
+    <Container>
+      <Row>
+        <h1>Add New Game Session</h1>
+      </Row>
+      <Row>
+        <h3>Players to Add </h3>
+      </Row>
+      <Row>
+        <h3>Players in Game</h3>
+      </Row>
+    </Container>
+  );
 };
 
 export default AddNewGame;
