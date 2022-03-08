7020893beea7c2436faefee491cf1876981a844e
Sakib Ahmed <sakib.ahmed@onepeloton.com>
2022-03-08 16:16:18 -0500

    back btn

--- src/Components/AddNewGame.js
+++ src/Components/AddNewGame.js
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import { getUsers } from "../Firebase/PokerApi";
+import { useNavigate } from "react-router-dom";
 
 const AddNewGame = () => {
+  const [users, setUsers] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    getUsers().then((snapshot) => {
+      const keys = Object.keys(snapshot.val());
+      const _users = [];
+      for (const element of keys) {
+        _users.push(snapshot.val()[element]);
+      }
+      setUsers(_users);
+    });
+  }, [users]);
+
   return (
     <Container>
       <Row>
@@ -13,6 +31,13 @@ const AddNewGame = () => {
       </Row>
       <Row>
         <h3>Players in Game</h3>
+      </Row>
+      <Row>
+        <Col xs={1}>
+          <Button onClick={() => navigate(-1)} className="blackBtn">
+            Back
+          </Button>
+        </Col>
       </Row>
     </Container>
   );
