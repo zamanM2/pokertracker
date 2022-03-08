ed8b4f52ce7f5a49249c39978b3055e9ce046173
Sakib Ahmed <sakib.ahmed@onepeloton.com>
2022-03-08 13:38:41 -0500

    bootstrap

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import React from "react";
 import "./App.css";
 import EarningsGraph from "./Components/EarningsGraph";
 import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
 import UserList from "./Components/UserList";
 
 function App() {
   return (
     <Container>
-      <EarningsGraph />
-      <UserList />
+      <Row>
+        <Col>
+          <EarningsGraph />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <UserList />
+        </Col>
+      </Row>
     </Container>
   );
 }
