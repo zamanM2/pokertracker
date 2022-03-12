362442bacbfea22b424001b4b79d10e754d5ed74
Sakib Ahmed <sakib.ahmed@onepeloton.com>
2022-03-12 02:25:31 -0500

    fix

--- src/Firebase/PokerApi.js
+++ src/Firebase/PokerApi.js
@@ -1,5 +1,5 @@
 import { dbRef } from "./FirebaseConfig";
-import { child, get, push, remove, update } from "firebase/database";
+import { child, get, push, update } from "firebase/database";
 
 export const getUsers = () => {
   return get(child(dbRef, `/users/`));
