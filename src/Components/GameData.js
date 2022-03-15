df971546d1a8469158822c9209cf61b1004ca745
Sakib Ahmed <sakib.ahmed@onepeloton.com>
2022-03-15 13:04:20 -0400

    upload

--- src/Components/GameData.js
+++ src/Components/GameData.js
@@ -1,6 +1,13 @@
 import React, { useEffect, useState } from "react";
 import Container from "react-bootstrap/Container";
-import { getGameData, getGameImage } from "../Firebase/PokerApi";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import {
+  getGameData,
+  getGameImage,
+  uploadGameImage,
+} from "../Firebase/PokerApi";
 import { useParams, useNavigate } from "react-router-dom";
 import Button from "react-bootstrap/Button";
 import { IoMdArrowBack } from "react-icons/io";
@@ -9,6 +16,7 @@ import { formatDate, earningsCompare } from "../utils/utils";
 const GameData = () => {
   const [gameData, setGameData] = useState([]);
   const [gameImage, setGameImage] = useState("");
+  const [imageToUpload, setImageToUpload] = useState({});
   let { date } = useParams();
   const navigate = useNavigate();
 
@@ -29,6 +37,18 @@ const GameData = () => {
     }
     getImage();
   }, []);
+
+  const handleChange = (e) => {
+    if (e.target.files[0]) {
+      setImageToUpload(e.target.files[0]);
+    }
+  };
+
+  const handleUpload = () => {
+    uploadGameImage(date, imageToUpload).then(async () => {
+      setGameImage(await getGameImage(date));
+    });
+  };
 
   return (
     <Container>
@@ -58,11 +78,26 @@ const GameData = () => {
       <img
         style={{ width: "100%", height: "100%", marginBottom: "10px" }}
         src={gameImage}
-        alt="Girl in a jacket"
+        alt="Game Picture"
       />
-      <Button style={{ marginBottom: "10px" }} onClick={() => navigate("/")}>
-        <IoMdArrowBack />
-      </Button>
+      <Row>
+        <Col xs={7}>
+          <Form.Control type="file" onChange={handleChange} />
+        </Col>
+        <Col>
+          <Button onClick={handleUpload}>Upload</Button>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Button
+            style={{ marginBottom: "10px" }}
+            onClick={() => navigate("/")}
+          >
+            <IoMdArrowBack />
+          </Button>
+        </Col>
+      </Row>
     </Container>
   );
 };
--- src/Firebase/PokerApi.js
+++ src/Firebase/PokerApi.js
@@ -1,6 +1,6 @@
 import { dbRef, storage } from "./FirebaseConfig";
 import { child, get, push, update } from "firebase/database";
-import { ref, getDownloadURL } from "firebase/storage";
+import { ref, getDownloadURL, uploadBytesResumable } from "firebase/storage";
 
 export const getUsers = () => {
   return get(child(dbRef, `/users/`));
@@ -55,4 +55,31 @@ export const saveGameSession = async (date, usersInGame) => {
 export const getGameImage = async (date) => {
   const gameRef = ref(storage, `gameImages/${date}.jpg`);
   return getDownloadURL(gameRef);
+};
+
+export const uploadGameImage = async (date, file) => {
+  const storageRef = ref(storage, `gameImages/${date}.jpg`);
+  const uploadTask = uploadBytesResumable(storageRef, file);
+
+  uploadTask.on(
+    "state_changed",
+    (snapshot) => {
+      const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      console.log("Upload is " + progress + "% done");
+      switch (snapshot.state) {
+        case "paused":
+          console.log("Upload is paused");
+          break;
+        case "running":
+          console.log("Upload is running");
+          break;
+      }
+    },
+    (error) => {},
+    () => {
+      getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+        console.log(downloadURL);
+      });
+    }
+  );
 };
