4bc6cd33963b047acba2239cc0ee2e42aae15d57
Mohammad Badruzzaman <mohammad.badruzz@gmail.com>
2022-03-15 22:03:45 -0400

    comment out

--- src/Firebase/FirebaseConfig.js
+++ src/Firebase/FirebaseConfig.js
@@ -12,6 +12,16 @@ const firebaseConfig = {
   appId: "1:438544242576:web:a23eac0726d7bf2d26c515",
 };
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyD2YuoUUJRFa23BSXtBJFWTs42vWo8IxAg",
+//   authDomain: "poker-testdb.firebaseapp.com",
+//   projectId: "poker-testdb",
+//   storageBucket: "poker-testdb.appspot.com",
+//   messagingSenderId: "366716803608",
+//   appId: "1:366716803608:web:7e5203292a04d8aab4816d",
+//   measurementId: "G-0MNSFB2X59",
+// };
+
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 const dbRef = ref(db);
