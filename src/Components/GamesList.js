2a3b410f3f17511322c2d8bd8e1098523fe9d87c
Sakib Ahmed <sakib.ahmed@onepeloton.com>
2022-10-30 12:10:30 -0400

    gamelist

--- src/Components/GamesList.js
+++ src/Components/GamesList.js
@@ -8,19 +8,22 @@ import { Link } from "react-router-dom";
 import { getGameSessions } from "../Firebase/PokerApi";
 import { dateCompare, formatDate } from "../utils/utils";
 import "../css/blackBtn.css";
+import Accordion from "react-bootstrap/Accordion";
 
 const GamesList = () => {
   const [gameSessions, setGameSessions] = useState([]);
+  const [numberOfSeasons, setNumberOfSeasons] = useState([]);
 
   useEffect(() => {
     //populate state with an array of objects {date, season}
     getGameSessions().then((snapshot) => {
       const seasons = Object.keys(snapshot.val()); //seasons
+      setNumberOfSeasons([...Array(seasons.length).keys()].reverse());
       let _gameSessions = [];
       for (let i = 0; i < seasons.length; i++) {
         let dates = Object.keys(snapshot.val()[seasons[i]]);
         for (let j = 0; j < dates.length; j++) {
-          _gameSessions.push({ date: dates[j], season: seasons[i] });
+          _gameSessions.push({ date: dates[j], season: i });
         }
       }
       setGameSessions(_gameSessions.sort(dateCompare));
@@ -28,7 +31,7 @@ const GamesList = () => {
   }, []);
 
   return (
-    <Container>
+    <Container style={{ paddingRight: "5px" }}>
       <Row>
         <Col>
           <h2>
@@ -42,15 +45,29 @@ const GamesList = () => {
         </Col>
       </Row>
       <Row>
-        {[...gameSessions].map((game) => (
-          <Link
-            style={{ marginBottom: "3px", color: "black" }}
-            key={game.date}
-            to={`/${game.season}/${game.date}`}
-          >
-            {formatDate(game.date)}
-          </Link>
-        ))}
+        <Accordion>
+          {[...numberOfSeasons].map((season) => (
+            <Accordion.Item eventKey={season.toString()}>
+              <Accordion.Header>{`Season ${season}`}</Accordion.Header>
+              <Accordion.Body>
+                {[...gameSessions]
+                  .filter((game) => game.season === season)
+                  .map((game) => (
+                    <>
+                      <Link
+                        style={{ marginBottom: "3px", color: "black" }}
+                        key={game.date}
+                        to={`/season-${game.season}/${game.date}`}
+                      >
+                        {formatDate(game.date)}
+                      </Link>
+                      <br />
+                    </>
+                  ))}
+              </Accordion.Body>
+            </Accordion.Item>
+          ))}
+        </Accordion>
       </Row>
     </Container>
   );
