32523418aed715419fea777c3967575556076a1e
Sakib Ahmed <sakib.ahmed@onepeloton.com>
2022-11-03 22:49:29 -0400

    legal

--- src/Components/LegalPage.js
+++ src/Components/LegalPage.js
@@ -20,19 +20,15 @@ const LegalPage = () => {
       >
         <IoMdArrowBack />
       </Button>
-      <h2>Game Fees</h2>
+      <h1 style={{ textAlign: "center" }}>
+        <i>Legal Terms</i>
+      </h1>
+      <h2>[A]rgoBros&copy; Seasons</h2>
       <p>
-        At the start of the Season, each player will donate $5 to the prize
-        pool. At the end of each game, each player will donate $1 to the prize
-        pool.
+        Welcome to [A]rgoBros&copy; Seasons. Each season has 12 games. At the
+        end of the season, winners are determined by the number of points
+        earned.
       </p>
-      <h2>Bank Rules</h2>
-      <p>
-        Sakib will be bank every night. When sending and requesting money on
-        Venmo/Zelle, he'll add or subtract $1 from each player (depending on if
-        you owe or are owed $). This money will be added to the prize pool.
-      </p>
-
       <h2>Point System</h2>
       <ul style={{ listStyleType: "square" }}>
         <li>
@@ -45,19 +41,34 @@ const LegalPage = () => {
         </li>
         <li>Most money in one game session +2 points</li>
         <li>Longest win streak +1 point</li>
-        <li>Best +/- ratio +1 point</li>
+        <li>Best +/- ratio (%) +1 point</li>
       </ul>
       <div>
         ** In case of a tie, the person with the most overall season earnings is
         the winner
       </div>
       <br />
-      <h2>Prizes for Season 1</h2>
+      <h2>Game Fees</h2>
+      <p>
+        At the start of the Season, each player will donate $5 to the prize
+        pool. At the end of each game, each player will donate $1 to the prize
+        pool.
+      </p>
+      <h2>Bank Rules</h2>
+      <p>
+        Sakib will be bank every night. When sending and requesting money on
+        Venmo/Zelle, he'll add or subtract $1 from each player (depending on if
+        you owe or are owed $). This money will be added to the prize pool.
+      </p>
+      <h2>Prizes for the Season</h2>
       <ul style={{ listStyleType: "square" }}>
         <li>First Place: 50% of Prize pool and a 1st Place trophy</li>
         <li>Second Place: 30% of Prize pool</li>
         <li>Third Place: 20% of Prize pool</li>
       </ul>
+      <div>
+        ** A minimum of 8 games have to be played to be considered for prizes
+      </div>
     </Container>
   );
 };
