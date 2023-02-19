af3fdac13900903c2ea1c4b339367f0c04b53639
Sakib Ahmed <sakib.ahmed@onepeloton.com>
2023-02-19 18:04:03 -0500

    Player stats model

---
+++ src/DataModels/PlayerStatsModel.js
@@ -0,0 +1,10 @@
+class PlayerStatsModel {
+  constructor(name, seasonEarnings, allIns, points) {
+    this.name = name;
+    this.seasonEarnings = seasonEarnings;
+    this.allIns = allIns;
+    this.points = points;
+  }
+}
+
+export default PlayerStatsModel;
